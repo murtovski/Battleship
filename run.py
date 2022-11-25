ffc9432534a9f76e5a418b2791e4f23a05fc8fde
murtovski <murta1991@gmail.com>
2022-11-25 13:41:19 +0000

    Created the hit ships method, need to create game and record moves.

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ class GameBoard:
     def __init__(self, board):
         self.board = board
 
-    def change_letters_to_nums(self):
+    def change_letters_to_nums():
         change_letters = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
         return change_letters
 
@@ -31,8 +31,8 @@ class Ship:
     def __init__(self, board):
         self.board = board
 
-
     def create_ships(self):
+        #this loop will validate the ship locations and apply the ship to a random spot on the board
         for i in range(5):
             self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
             while self.board[self.x_row][self.y_column]== "X":
@@ -40,11 +40,26 @@ class Ship:
             self.board[self.x_row][self.y_column] = "X"
         return self.board
     
-
     def user_input(self):
         try:
             x_row = input("Enter the row of the ship: ")
             while x_row not in '12345678':
                 print("not an appropriate choice, please select a row between 1 and 8")
                 x_row = input("Enter the row of the ship: ")
-            
+
+            y_column = input("Enter the column letter of the ship: ").upper()
+            while y_column not in "ABCDEFGH":
+                print("Not an appropriate range, please select a letter from A to G") 
+                y_column = input("Enter the column letter of the ship: ").upper()
+            return int(x_row) - 1, GameBoard.change_letters_to_nums()[y_column]
+        except ValueError and KeyError:
+            print("Not a valid input")
+            return self.user_input()
+        
+    def hit_ships(self):
+        hit_ships = 0
+        for row in self.board:
+            for column in row:
+                if column == "X":
+                    hit_ships += 1
+        return hit_ships
