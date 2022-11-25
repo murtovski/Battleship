9c0752211935bed3250aad8968628648462af975
murtovski <murta1991@gmail.com>
2022-11-25 12:44:57 +0000

    created the gameboard class

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+class GameBoard:
+    def __init__(self, board):
+        self.board = board
+
+    def change_letters_to_nums():
+        change_letters = {"A":0 , "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7}
+        return change_letters
+
+    def print_board(self):
+        print("  A B C D E F G H")
+        print("  ---------------")
+        row_number = 1
+        for row in self.board:
+            print(f"  {row_number} |".join(row))
+            row_number +=1
+
+
