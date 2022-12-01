c4f09bb8be93fd62c3e760250ee686e4406bce5b
murtovski <murta1991@gmail.com>
2022-12-01 17:02:24 +0000

    Tested the game and resolved a bug where only one computer ship was generating

--- run.py
+++ run.py
@@ -55,7 +55,6 @@ class Ship:
 
     def user_input(self):
         try:
-            print("PLAYER'S BOARD")
             x_row = input("Enter the row of the ship: ")
             while x_row not in '12345678' or x_row == "":
                 print("not an appropriate choice, please select a row between 1 and 8")
@@ -84,19 +83,13 @@ class Ship:
         return hit_ships
 
 def player_move(row, col, board, score, location):
-    print(location)
     split = list(map(list, zip(*location)))
     lat, long = split
-    print(lat)
-    print(long)
     #Checks for duplicate
     while board.board[row][col] == "-" or board.board[row][col] == "@":
         print("You have made that guess already")
         row, col = Ship.user_input(object)
-    #checks for hits
-   # for i in lat:
-        #for j in long:
-    if row == int(lat[i]) and col == int(long[j]):
+    if row in lat and col in long:
         print("You sunk a Battleship!")
         board.board[row][col] = "@"
         score += 1
@@ -128,25 +121,31 @@ def RunGame():
     player_board = GameBoard([[" "] * 8 for i in range(8)])
     Ship.create_ships(player_board)
     computer_location = Ship.create_computer_ships()
-    print(computer_location)
     player_score = 0
     computer_score = 0
+    print("\n")
     print("WELCOME TO BATTLESHIP!")
     print("Make your move and wait for the computer to make their move")
-    print("First to score fove hits wins!")
+    print("First to score five hits wins!")
+    print("\n")
     
     #Starts turns
     while player_score < 5 and computer_score < 5:
         GameBoard.print_board(computer_board)
         print("COMPUTER'S BOARD")
+        print(f"The computer's score is {computer_score}")
         print("----------------------------------------")
         GameBoard.print_board(player_board)
+        print("PLAYER'S BOARD")
+        print(f"Your score is {player_score}")
+        print("----------------------------------------")
+        print("\n")
         computer_row, computer_col = Ship.user_input(object)
         player_score = player_move(computer_row, computer_col, computer_board, player_score, computer_location)
-        print(f"Your score is {player_score}")
+        
         player_row, player_col = Ship.computer_input(object)
         computer_score = computer_move(player_row, player_col, player_board, computer_score)
-        print(f"The computer's score is {computer_score}")
+        
     if computer_score == 5:
         print("The computer sank all your ships")
     else:
